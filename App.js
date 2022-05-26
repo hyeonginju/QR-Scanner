2d95d08bfef63fba352e291aaf82af132dd541c3
hyeonginju <guddls64@naver.com>
2022-05-26 16:14:27 +0900

    first

---
+++ App.js
@@ -0,0 +1,110 @@
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
+import {RNCamera} from 'react-native-camera';
+import Tts from 'react-native-tts';
+
+function App() {
+  const [barcode, setBarcode] = useState(null);
+  const [tmp, setTmp] = useState(null);
+
+  if (barcode != null && barcode.type == 'QR_CODE') {
+    if (tmp != barcode.data && barcode.data.startsWith('eyesharenet')) {
+      Tts.stop();
+      Tts.speak(barcode.data);
+    }
+    setTmp(barcode.data);
+    setBarcode(null);
+  }
+
+  return (
+    <View style={styles.screen}>
+      <SafeAreaView style={styles.saveArea}>
+        <View style={styles.topBar}>
+          <Text style={styles.topBarTitleText}>Eye Share Net</Text>
+        </View>
+      </SafeAreaView>
+
+      <View style={styles.caption}>
+        <Text style={styles.captionTitleText}>hi</Text>
+      </View>
+
+      <RNCamera style={styles.rnCamera} onBarCodeRead={setBarcode} />
+
+      <View style={styles.cameraControl}>
+        <TouchableOpacity style={styles.btn} onPress={() => setBarcode(null)}>
+          <Text style={styles.btnText}>New QR Scan</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: '#F2F2FC',
+  },
+  saveArea: {
+    backgroundColor: '#62d1bc',
+  },
+  topBar: {
+    height: 50,
+    backgroundColor: '#62d1bc',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  topBarTitleText: {
+    color: '#ffffff',
+    fontSize: 20,
+  },
+  caption: {
+    height: 120,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  captionTitleText: {
+    color: '#121B0D',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  btn: {
+    width: 240,
+    borderRadius: 4,
+    backgroundColor: '#62d1bc',
+    paddingHorizontal: 24,
+    paddingVertical: 12,
+    marginVertical: 8,
+  },
+  btnText: {
+    fontSize: 18,
+    color: '#ffffff',
+    textAlign: 'center',
+  },
+  rnCamera: {
+    flex: 1,
+    width: '94%',
+    alignSelf: 'center',
+  },
+  rmCameraResult: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eeeeee',
+  },
+  rmCameraResultText: {
+    fontSize: 20,
+    color: '#62d1bc',
+  },
+  cameraControl: {
+    height: 180,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
